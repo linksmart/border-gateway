c9e86686be7fff6b73fa9c114c3715ed1748a6af
hareeqi <hareeqi@gmail.com>
2017-03-24 13:21:47 +0100

    first commit and inital work

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+const net = require('net');
+const Proxy =  require('tcp-proxy')
+const insubnet = require('insubnet')
+const configs = require('./config.json')
+
+
+configs.servers.forEach((server) => {
+  const external_interface = net.createServer(function(client) {
+
+    const proxy = new Proxy();
+    const options = { target: { host:server.dest_address, port:server.dest_port }};
+
+    if(configs.private_BGW && !insubnet.Validate(client.remoteAddress,configs.allowed_addresses)){
+      console.log('This is a private BGW: illegal connection made by %s',client.remoteAddress);
+      client.destroy()
+    } else {
+      proxy.proxy(client,options)
+    }
+
+  })
+  external_interface.listen(server.port,configs.bind_address);
+  console.log("Forwarding %s port: %d to %s:%d",server.name,server.port,server.dest_address,server.dest_port);
+})
