54dc39113e8f8c68bc443d52860b77faf76df3f7
hareeqi <hareeqi@gmail.com>
2017-03-31 18:05:15 +0200

    log after bind

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ const mqtt = require('mqtt-packet')
 
 const server = net.createServer((srcClient)=> {
 
-  const dstClient = net.connect(config.default_broker.port, config.default_broker.address, ()=> {
+  const dstClient = net.connect(config.broker.port, config.broker.address);
 
-    const srcParser = mqtt.parser()
-    const dstParser = mqtt.parser()
 
-    srcClient.on('data',(data)=>srcParser.parse(data))
-    dstClient.on('data',(data)=>dstParser.parse(data))
+  const srcParser = mqtt.parser()
+  const dstParser = mqtt.parser()
+  srcClient.on('data',(data)=>srcParser.parse(data))
+  dstClient.on('data',(data)=>config.broker.authorize_response?dstParser.parse(data):srcClient.write(data))
+  dstClient.on('error',()=>{srcClient.destroy();dstClient.destroy()})
+  srcClient.on('error',()=>{srcClient.destroy();dstClient.destroy()})
 
-    srcParser.on('packet',(packet)=> {
-      //console.log("%c client",'color:blue');
-      //console.log(packet);
-      dstClient.write(mqtt.generate(packet))
-    })
-    dstParser.on('packet',(packet)=>{
-      //console.log("%c server",'color:green');
-      //console.log(packet);
-      srcClient.write(mqtt.generate(packet))
-    })
-  });
-
+  srcParser.on('packet',(packet)=> {
+    //console.log("%c client",'color:blue');
+    //console.log(packet);
+    dstClient.write(mqtt.generate(packet))
+  })
+  dstParser.on('packet',(packet)=>{
+    //console.log("%c server",'color:green');
+    //console.log(packet);
+    srcClient.write(mqtt.generate(packet))
+  })
 });
 
 
-server.listen(config.bind_port, config.bind_port);
-console.log("iot-bgw-mqtt-proxy listening on %s %d ",config.bind_address,  config.bind_port)
+server.listen(config.bind_port, config.bind_port,()=>
+console.log("iot-bgw-mqtt-proxy listening on %s:%d ",config.bind_address,  config.bind_port));
