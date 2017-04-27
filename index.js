6180467b429a71e070ae16af4aea7b2dcb6cc066
hareeqi <hareeqi@gmail.com>
2017-04-27 19:40:08 +0200

    bla

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 try {
   // this is used during devolopent and outside docker
   module.exports  = require('../../iot-bgw-aaa-client/main.js')
+  console.log('taking local file');
 } catch (e){
   module.exports  = require('./main.js')
+  console.log('taking remote file');
+
 }
