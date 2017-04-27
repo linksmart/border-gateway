8571d83fddc408d292c8e09b0a92d5822069e542
hareeqi <hareeqi@gmail.com>
2017-04-27 19:26:22 +0200

    rename to iot-bgw-aaa-client

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 try {
+  // this is used during devolopent and outside docker
   module.exports  = require('../../iot-bgw-auth-client/main.js')
 } catch (e){
   module.exports  = require('./main.js')
