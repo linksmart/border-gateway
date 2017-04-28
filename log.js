f7931a04d418550f97fc89e9945ff43f133b2560
hareeqi <hareeqi@gmail.com>
2017-04-28 18:32:12 +0200

    forever w color

--- log.js
+++ log.js
@@ -1,13 +1,27 @@
+const chalk = require('chalk');
+const config = require('./config_mgr')();
 
-
-const log = (...arg)=> console.log("AAA", ...arg);
-
-
+let process_color = {
+  http:chalk.bgCyan,
+  mqtt: chalk.bgBlue,
+  external:chalk.bgGreen,
+  auth:chalk.bgMagenta
+}
+process_color = process_color[config.aaa_client.name.split('-')[0]]
+let cat_color = {
+  "W":chalk.yellow,
+  "F":chalk.red,
+  "E":chalk.red
+}
+const logNoColor = (...arg)=> console.log(config.aaa_client.name,"AAA", ...arg);
+const logColor =  (cat, ...arg)=> console.log(process_color(config.aaa_client.name,"AAA"), cat_color[cat[0]]?cat_color[cat[0]](cat, ...arg):chalk.reset(cat,...arg));
+const log = config.aaa_client.no_color ? logNoColor:logColor
 
 const CAT = {
-  PROCESS: "PROCESS",
+  PROCESS_START: "I PROCESS_START",
   BUG: "F BUG",
   RULE_ALLOW:'I RULE_ALLOW',
+  DISCONNECT_CON:'W DISCONNECT_CON',
   RULE_DENY:'W RULE_DENY',
   PROFILE: 'W PROFILE',
   PASSWORD: 'W PASSWORD',
