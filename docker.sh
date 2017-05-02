e8d9721a91d6436973ae2f4fd5aea204798603aa
hareeqi <hareeqi@gmail.com>
2017-05-03 00:46:24 +0200

    socker sh ||

--- docker.sh
+++ docker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 dontexit = true
-trap 'echo "existing docker..."; ./node_modules/.bin/forever stopall ; $dontexit=false; echo "dont exist false"' INT
+trap './node_modules/.bin/forever stopall ; let dontexit=false' INT
 
-
-#npm run $1 &
 ./node_modules/.bin/forever run$1.json --colors dotenv_config_path=./config/config$1.env &
 
 while $dontexit
