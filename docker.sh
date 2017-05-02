e5d8c2b2a2e8ff7f1ecb378d5f11dbe8b433af66
hareeqi <hareeqi@gmail.com>
2017-05-03 00:23:53 +0200

    socker sh ||

--- docker.sh
+++ docker.sh
@@ -3,8 +3,8 @@
 trap 'echo "existing docker..."; ./node_modules/.bin/forever stopall ; exit 0' INT
 
 
-npm run $1 &
-./node_modules/.bin/forever run$1.json --colors dotenv_config_path=./config/config$1.env
+#npm run $1 &
+./node_modules/.bin/forever run$1.json --colors dotenv_config_path=./config/config$1.env &
 
 while true
 do
