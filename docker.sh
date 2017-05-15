22558e20a6b47684c322deabbd16589383178e1e
Farshid Tavakolizadeh <email@farshid.ws>
2017-05-15 14:15:58 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,8 +2,7 @@
 
 trap './node_modules/.bin/forever stopall ; exit 0' INT
 
-node json2env.js
-
+node json2env.js && \
 ./node_modules/.bin/forever --minUptime=1000 --spinSleepTime=1000 run$1.json --colors  dotenv_config_path=./node_modules/config.env &
 
 while true
