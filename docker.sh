814349e2bda046d08090ff35412b54173098af61
hareeqi <hareeqi@users.noreply.github.com>
2017-05-16 18:02:08 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ $1 = "build" ]; then
+    echo Building the dependencies for all components...
+    npm install --prefix ./dev/iot-bgw-external-interface ./dev/iot-bgw-external-interface 
+    npm install --prefix ./dev/iot-bgw-auth-server ./dev/iot-bgw-auth-server 
+    npm install --prefix ./dev/iot-bgw-mqtt-proxy ./dev/iot-bgw-mqtt-proxy 
+    npm install --prefix ./dev/iot-bgw-http-proxy ./dev/iot-bgw-http-proxy 
+    npm install --prefix ./dev/iot-bgw-aaa-client ./dev/iot-bgw-aaa-client 
+    echo Finished building the dependencies for all components
+    exit 0
+fi
+
 trap './node_modules/.bin/forever stopall ; exit 0' INT
 
 node json2env.js && \
