d39182214ec9623f6b9a7decb8632635f07b4440
hareeqi <hareeqi@gmail.com>
2017-07-18 21:32:57 +0200

    build script checkout master

--- docker.sh
+++ docker.sh
@@ -3,11 +3,11 @@
 if [ "$1" = "build" ]; then
     echo Building the dependencies for all components...
     npm install --only=dev
-    cd dev/iot-bgw-external-interface && npm install && cd ../..
-    cd dev/iot-bgw-auth-server && npm install && cd ../..
-    cd dev/iot-bgw-mqtt-proxy && npm install && cd ../..
-    cd dev/iot-bgw-http-proxy && npm install && cd ../..
-    cd dev/iot-bgw-aaa-client && npm install && cd ../..
+    cd dev/iot-bgw-external-interface && git checkout master && npm install && cd ../..
+    cd dev/iot-bgw-auth-server && git checkout master && npm install && cd ../..
+    cd dev/iot-bgw-mqtt-proxy && git checkout master && npm install && cd ../..
+    cd dev/iot-bgw-http-proxy && git checkout master && npm install && cd ../..
+    cd dev/iot-bgw-aaa-client && git checkout master && npm install && cd ../..
     echo Finished building the dependencies for all components
     exit 0
 fi
@@ -22,7 +22,7 @@ elif [ "$1" = "http2https" ]; then
 
     node json2env.js && \
     node -r dotenv/config http2https.js dotenv_config_path=./node_modules/config.env
-    
+
 else
 
     trap './node_modules/.bin/forever stopall ; exit 0' INT
