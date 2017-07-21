a5da24a5d39b50b72add79db954dee27c1594b4e
hareeqi <hareeqi@users.noreply.github.com>
2017-07-21 10:06:03 +0200

    Update bgw.sh

--- bgw.sh
+++ bgw.sh
@@ -79,7 +79,7 @@ elif [ "$1" = "forever" ]; then
     trap './node_modules/.bin/forever stopall ; exit 0' INT
 
     node json2env.js && \
-    ./node_modules/.bin/forever --minUptime=1000 --spinSleepTime=1000 run$1.json --colors  dotenv_config_path=./node_modules/config.env &
+    ./node_modules/.bin/forever --minUptime=1000 --spinSleepTime=1000 forever.json --colors  dotenv_config_path=./node_modules/config.env &
 
     while true
     do
