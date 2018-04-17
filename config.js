e2f890ff3835ae66942b0d8be2206e2dd99d85ef
Jannis Warnat <jannis.warnat@fit.fraunhofer.de>
2018-04-17 08:43:53 +0200

    debug

--- config.js
+++ config.js
@@ -61,6 +61,5 @@ let config = {
     }
 };
 
-//require('../iot-bgw-aaa-client').init("HTTP_PROXY", config);
-require('../iot-bgw-aaa-client').init("HP_PROXY", config);
+require('../iot-bgw-aaa-client').init("HTTP_PROXY", config);
 module.exports = config;
