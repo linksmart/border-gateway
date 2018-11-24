61610a307bc92f2d79fcc51d3c73351337514978
jannis.warnat <jannis.warnat@fit.fraunhofer.de>
2018-11-24 13:54:01 +0100

    fix auth_type none

--- bgw-auth-service/request_auth.js
+++ bgw-auth-service/request_auth.js
@@ -1,33 +1,40 @@
+const config = require("./config");
 const validate = require("./validate");
 const decode64 = (b64) => new Buffer(b64, 'base64').toString('ascii');
 
 const requestAuth = async (req) => {
 
-        let auth_type = "password";
-        let username = "anonymous";
-        let password = "anonymous";
+    let override_conf = {};
 
-        if (req.headers && req.headers.authorization) {
-            let parts = req.headers.authorization.split(' ');
-            if (parts.length === 2) {
-                if ((parts[0] === 'Bearer' || parts[0] === 'bearer') && (username = parts[1])) {
-                    parts = username.split(":");
-                    password = parts[1];
-                    auth_type = 'access_token';
-                }
+    if (req.bgw.alias && req.bgw.alias.override_aaa_client_config) {
+        override_conf = req.bgw.alias.override_aaa_client_config
+    }
 
-                else if (parts[0] === 'Basic' && (username = decode64(parts[1]))) {
-                    parts = username.split(":");
-                    username = parts[0];
-                    password = parts[1];
-                    auth_type = 'password';
-                }
+    let auth_type = override_conf.openid_authentication_type || config.aaa_client.openid_authentication_type || 'password';
+
+    let username = "anonymous";
+    let password = "anonymous";
+
+    if (!(auth_type === 'none') && req.headers && req.headers.authorization) {
+        let parts = req.headers.authorization.split(' ');
+        if (parts.length === 2) {
+            if ((parts[0] === 'Bearer' || parts[0] === 'bearer') && (username = parts[1])) {
+                parts = username.split(":");
+                password = parts[1];
+                auth_type = 'access_token';
+            }
+
+            else if (parts[0] === 'Basic' && (username = decode64(parts[1]))) {
+                parts = username.split(":");
+                username = parts[0];
+                password = parts[1];
+                auth_type = 'password';
             }
         }
+    }
 
-        let override_conf = {};
-        override_conf.openid_authentication_type = auth_type;
-        return await validate(req.body.input, `[source:${req.connection.remoteAddress}:${req.connection.remotePort}]`, username, password, override_conf);
+    override_conf.openid_authentication_type = auth_type;
+    return await validate(req.body.input, `[source:${req.connection.remoteAddress}:${req.connection.remotePort}]`, username, password, override_conf);
 };
 
 
