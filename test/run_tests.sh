490720fe62d7cf5bc6ac9b329170f427b674e748
jannis.warnat <jannis.warnat@fit.fraunhofer.de>
2019-06-17 12:11:04 +0200

    Introduce testing in Travis

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -44,6 +44,9 @@ fi
 
 # Start backend (Mosquitto, Service Catalog, Redis)
 cd "$scriptDir/backend"
+docker volume create --name=pgdata
+docker-compose up -d keycloak
+sleep 30
 docker-compose up -d
 
 CA=$1
